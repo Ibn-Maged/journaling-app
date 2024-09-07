4a9d34fa03d9ed0fb458f273bb54682214759214
Ibn-Maged <mohamedmaged1607@gmail.com>
2024-09-07 22:17:00 +0300

    Added add post functionality

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from 'express';
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-const posts = [
-    {title: "Post 1", content: "Content 1"},
-    {title: "Post 2", content: "Content 2"}
-];
+const posts = [];
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) =>{
     res.render("index.ejs", {posts: posts});
@@ -24,6 +23,12 @@ app.get("/contact", (req, res) => {
 
 app.get("/post/new", (req, res) => {
     res.render("new_post.ejs");
+});
+
+app.post("/post/add", (req, res) => {
+    const new_post = {title: req.body["title"], content: req.body["content"]};
+    posts.push(new_post);
+    res.redirect("/");
 });
 
 app.listen(port, () => {
