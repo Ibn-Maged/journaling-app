6d56a3d4cde0fe1553835dbbad452f5d8e7b413a
Ibn-Maged <mohamedmaged1607@gmail.com>
2024-09-07 21:27:00 +0300

    Added the main routes

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (request, response) =>{
-    response.render("index.ejs");
+app.get("/", (req, res) =>{
+    res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact_us.ejs");
+});
+
+app.get("/post/new", (req, res) => {
+    res.render("new_post.ejs");
 });
 
 app.listen(port, () => {
